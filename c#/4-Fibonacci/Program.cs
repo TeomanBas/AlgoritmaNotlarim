1b913c538280946f36c44f4c2283c5a927357be6
Teoman BAŞ <teomanbas@protonmail.com>
2023-07-13 02:57:24 +0300

    Yeni uygulama eklendi

---
+++ c#/4-Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        fibo(18);
+    }
+    public static void fibo(int sayi)
+    {
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        Console.Write(a);
+        Console.Write(",");
+        Console.Write(b);
+        for (int i = 0; i <=sayi; i++)
+        {
+            Console.Write(",");
+            Console.Write(c);
+            a = b;
+            b = c ;
+            c = a + b;
+        }
+    }
+}
