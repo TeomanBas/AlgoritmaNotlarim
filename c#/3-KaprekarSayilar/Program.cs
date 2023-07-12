577c8bc8927e4bf5c2758186d60a44dea5bbf02a
Teoman BAŞ <teomanbas@protonmail.com>
2023-07-12 23:59:29 +0300

    kodlar düzenlendi.

--- c#/3-KaprekarSayilar/Program.cs
+++ c#/3-KaprekarSayilar/Program.cs
@@ -2,16 +2,12 @@
 {
     private static void Main(string[] args)
     {
-
         kaprekarkontrol(45);
-
     }
     public static bool kaprekarkontrol(int n)
     {
-
         // Kareyi hesapla
         int kare = n * n;
-
         // Basamak sayısını bul
         int basamak = 0;
         while (kare > 0)
@@ -19,10 +15,8 @@
             basamak++;
             kare /= 10;
         }
-
         // Kareyi tekrar hesapla
         kare = n * n;
-
         // Her basamak için deneme yap
         for (int i = 1; i < basamak; i++)
         {
@@ -37,9 +31,7 @@
                 Console.WriteLine("kaprekar sayı");
                 return true;
             }
-                
         }
-
         // Hiçbir basamak için Kaprekar sayısı olmadıysa false döndür
         Console.WriteLine("kaprekar sayı değil");
         return false;
